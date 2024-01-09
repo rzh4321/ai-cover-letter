7eac5223a09fdc6a4935f924218ab1a32827e450
Ricky <rzh4321@gmail.com>
2024-01-08 20:35:39 -0500

    finished footer

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import NavBar from "@/components/NavBar";
 import Hero from "@/components/Hero";
 import ImageSections from "@/components/ImageSections";
 import FAQ from "@/components/FAQ";
+import Footer from "@/components/Footer";
 import { useRef } from "react";
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
       <Hero exampleRef={ref} />
       <ImageSections exampleRef={ref} />
       <FAQ />
+      <Footer />
     </div>
   );
 }
